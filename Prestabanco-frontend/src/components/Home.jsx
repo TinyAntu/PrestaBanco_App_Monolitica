176ac1287b28a7f214f5c925af5e0eacc9073e66
Tiny Antu <alonso.henriquez.h@usach.cl>
2024-11-02 19:52:15 -0300

    Cosas de jenkins

--- Prestabanco-frontend/src/components/Home.jsx
+++ Prestabanco-frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         y a los ejecutivos aprobar o rechazar estas peticiones. Esta aplicación ha sido desarrollada usando tecnologías como{" "}
         <a href="https://spring.io/projects/spring-boot">Spring Boot</a> (para
         el backend), <a href="https://reactjs.org/">React</a> (para el Frontend)
-        y <a href="https://www.mysql.com/products/community/">MySQL</a> (para la
+        y <a href="https://www.postgresql.org/">Postgresql</a> (para la
         base de datos).
       </p>
     </div>
